00da13d123fbe12bc1298d34c2dce035bf376320
Faith Nyaboke <akiyafaith@gmail.com>
2023-10-05 23:01:31 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,23 +4,23 @@
 
 def canUnlockAll(boxes):
     """returns True if all boxes can be opened, else False"""
-# create a list to track of boxes that can be visited
+    # create a list to track of boxes that can be visited
 
 
-visited = [False] * len(boxes)
-visited = True
+    visited = [False] * len(boxes)
+    visited = True
 
-# a list for the keys you have
-keys = [0]
+    # a list for the keys you have
+    keys = [0]
 
-while False in visited and len(keys) > 0:
-    # Now get a key from the list
-    key = keys.pop()
-    box = boxes[key]
+    while False in visited and len(keys) > 0:
+        # Now get a key from the list
+        key = keys.pop()
+        box = boxes[key]
 
-    for new_key in box:
-        if new_key >= 0 and new_key < len(boxes) and not visited[new_key]:
-            visited[new_key] = True
-            keys.append(new_key)
+        for new_key in box:
+            if new_key >= 0 and new_key < len(boxes) and not visited[new_key]:
+                visited[new_key] = True
+                keys.append(new_key)
 
-return False not in visited
+    return False not in visited
