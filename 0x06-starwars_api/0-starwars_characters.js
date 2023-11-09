6096034a9488c6eeb45fed07e1bff9f9b99f79b3
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-10 00:30:38 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,28 @@
 #!/usr/bin/node
 const request = require('request');
 
-const Apiurl = 'https://swapi-api.alx-tools.com/api/films/';
 const movieId = process.argv[2];
-request(Apiurl + movieId, function (error, response, body) {
+
+if (!movieId) {
+  console.error('Usage: node 0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, async (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
-    printInOrder(characters);
+    await printInOrder(characters);
   }
 });
 
-async function printInOrder (characters) {
-  for (const character of characters) {
+async function printInOrder(characters) {
+  for (const characterUrl of characters) {
     await new Promise((resolve, reject) => {
-      request(character, function (error, response, body) {
+      request(characterUrl, (error, response, body) => {
         if (error) {
           reject(error);
         } else {
