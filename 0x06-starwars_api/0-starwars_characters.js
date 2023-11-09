4b6f5129c4a6ff7ecf4621099239575e92b6a4a5
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-10 00:42:22 +0300

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 
-const Apiurl = 'https://swapi-api.alx-tools.com/api/films/';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 const movieId = process.argv[2];
-request(Apiurl + movieId, (error, response, body) => {
+request(url + movieId, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
@@ -13,20 +13,16 @@ request(Apiurl + movieId, (error, response, body) => {
 });
 
 async function printInOrder(characters) {
-  characters.forEach(async (character) => {
-    try {
-      const characterBody = await new Promise((resolve, reject) => {
-        request(character, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(body);
-          }
-        });
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, _response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
       });
-      console.log(JSON.parse(characterBody).name);
-    } catch (error) {
-      console.error('Error fetching character data:', error);
-    }
-  });
+    });
+  }
 }
