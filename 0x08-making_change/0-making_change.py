06f80a277f2945f7b02e2bb3f05c23e40f936aeb
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-23 23:48:42 +0300

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    # Create a table, store the minimum number of coins needed for each amount
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Fill the table using dynamic programming
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    # If dp[total] is still infinite, it means the total cannot be achieved
+    return dp[total] if dp[total] != float('inf') else -1
