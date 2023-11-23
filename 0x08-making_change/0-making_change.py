03aff5a5377655613a74d5a7250442a5776a95f3
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-24 00:04:54 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""change comes from within"""
 def makeChange(coins, total):
+    """determine the fewest number of coins
+    needed to meet a given amount total."""
     if total <= 0:
         return 0
 
