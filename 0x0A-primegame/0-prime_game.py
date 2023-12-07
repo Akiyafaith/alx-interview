6e54b9f1ff85a52ec80d584e77915abaf1fdaf05
Faith Nyaboke <akiyafaith@gmail.com>
2023-12-08 01:08:31 +0300

    prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,11 +2,13 @@
 """Prime Game"""
 
 
-def is_prime(num):
+def is_prime(num, primes):
     """Check if a given number is prime."""
     if num < 2:
         return False
-    for i in range(2, int(num**0.5) + 1):
+    for i in primes:
+        if i * i > num:
+            break
         if num % i == 0:
             return False
     return True
@@ -16,8 +18,15 @@ def isWinner(x, nums):
     maria_wins = 0
     ben_wins = 0
 
+    max_num = max(nums)
+    primes = [2]  # Initialize with the first prime number
+
+    for num in range(3, max_num + 1, 2):
+        if is_prime(num, primes):
+            primes.append(num)
+
     for n in nums:
-        prime_count = sum(is_prime(i) for i in range(1, n + 1))
+        prime_count = sum(is_prime(i, primes) for i in range(1, n + 1))
         if prime_count % 2 == 0:
             ben_wins += 1
         else:
